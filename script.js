9466b5acceb69a349b8f42ffa05e606f41547d01
Jjoon0513 <ijjun244@gmail.com>
2025-03-30 22:31:57 +0900

    댬

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-require('dotenv').config();
+async function loadConfig() {
+    const response = await fetch('config.txt');
+    const value = await response.text();
+
+    return value;
+  }
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('API Key:', process.env.API_KEY);
     var calendarEl = document.getElementById('calendar');
     const today = new Date();
     const year = today.getFullYear();
+    
   
     var calendar = new FullCalendar.Calendar(calendarEl, {
       initialDate: `${year}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`,
@@ -29,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function fetchEventsForSchool(schoolCode, year, month) {
       const startDate = `${year}${String(month).padStart(2, '0')}01`;
       const endDate = `${year}${String(month).padStart(2, '0')}31`;
-      const url = `https://open.neis.go.kr/hub/SchoolSchedule?KEY=${process.env.API_KEY}&Type=xml&ATPT_OFCDC_SC_CODE=S10&SD_SCHUL_CODE=${schoolCode}&AA_FROM_YMD=${startDate}&AA_TO_YMD=${endDate}`;
+      const url = `https://open.neis.go.kr/hub/SchoolSchedule?KEY=${loadConfig()}&Type=xml&ATPT_OFCDC_SC_CODE=S10&SD_SCHUL_CODE=${schoolCode}&AA_FROM_YMD=${startDate}&AA_TO_YMD=${endDate}`;
   
       fetch(url)
         .then(response => response.text())
